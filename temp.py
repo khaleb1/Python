175e39f41f7f52c95d7c82619bc30860bb8df166
khaleb <131798095+khaleb1@users.noreply.github.com>
2025-07-29 10:48:14 +0000

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,14 @@
+
+
+unit = input("Is this tempeerature in Celsius or Fahrenh  eit (C/F) ") 
+temp = float(input("Enter the temperature: "))
+
+
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temperature in Farenheit is: {temp} \u00B0F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp} \u00B0C")
+else:
+    print(f"{unit} is an invalid unit of mrasurement ")
